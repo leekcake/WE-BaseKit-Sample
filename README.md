48b9d6a69f9eacd9ee2b4c6c4e97418c4296cd56
Blue Electric <leekcake@protonmail.com>
2019-09-14 23:53:23 +0900

    Update and Support WE-BaseKit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WE-BaseKit-Sample
 Sample for WE-BaseKit
+
+How to init
+
+1. Download Submodule (git submodule init / git submodule update)
+2. gradle build
+3. gradle execute
+4. Ready to go!
--- sample/jvm/src/main/kotlin/moe/leekcake/we/sample/BuilderLauncher.kt
+++ sample/jvm/src/main/kotlin/moe/leekcake/we/sample/BuilderLauncher.kt
@@ -4,6 +4,6 @@ import moe.leekcake.we.basekit.Builder
 import java.io.File
 
 fun main(args: Array<String>) {
-    val builder = Builder(SampleProject())
+    val builder = Builder(SampleProject(), "sample")
     builder.build( File(args[0]) )
 }
